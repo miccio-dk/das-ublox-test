bf2f39d7f8c01bcd2097fea26d2c0cd287971cf8
Riccardo Miccini <riccardo.miccini@gmail.com>
2015-06-27 23:08:21 +0200

    add db

--- web/index.php
+++ web/index.php
@@ -10,9 +10,21 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
+// Register the Twig templating engine
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path' => __DIR__.'/../views',
 ));
+
+// Register the Postgres database add-on
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+  array(
+    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+    'pdo.port' => $dbopts["port"],
+    'pdo.username' => $dbopts["user"],
+    'pdo.password' => $dbopts["pass"]
+  )
+);
 
 // Our web handlers
 
@@ -21,10 +33,28 @@ $app->get('/', function() use($app) {
   return 'Hello';
 });
 
-$app->get('/twig/{name}', function ($name) use ($app) {
-    return $app['twig']->render('index.twig', array(
-        'name' => $name,
-    ));
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
 });
 
+$app->get('/twig/{name}', function($name) use($app) {
+  return $app['twig']->render('index.twig', array(
+    'name' => $name,
+  ));
+});
+
+
 $app->run();
+
+?>
