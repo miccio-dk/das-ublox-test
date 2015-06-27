ae0fbd9fe82da098ad5eee62a786769c59e7119c
Riccardo Miccini <riccardo.miccini@gmail.com>
2015-06-27 22:00:50 +0200

    add twig

--- web/index.php
+++ web/index.php
@@ -10,11 +10,21 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+  'twig.path' => __DIR__.'/../views',
+));
+
 // Our web handlers
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return 'Hello';
+});
+
+$app->get('/twig/{name}', function ($name) use ($app) {
+    return $app['twig']->render('index.twig', array(
+        'name' => $name,
+    ));
 });
 
 $app->run();
