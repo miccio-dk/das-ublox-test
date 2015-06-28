d8d5e66c9e23567fcfe01975919c834f44cb4c34
Riccardo Miccini <riccardo.miccini@gmail.com>
2015-06-28 15:12:37 +0200

    more code

--- src/UBlox/GPSController.php
+++ src/UBlox/GPSController.php
@@ -20,11 +20,45 @@ class GPSController implements ControllerProviderInterface {
 			'UBlox\GPSController::postEntry'
 		);
 
+		$factory->get(
+			'/',
+			'UBlox\GPSController::getEntry'
+		);
+
+
+
 		return $factory;
 	}
 
 	public function postEntry(Application $app, Request $request) {
-		return json_encode($this->settings);
+		$gps_entry = array(
+			'coord'	=>array(
+				$request->get('la'),
+				$request->get('lo'),
+			),
+			'alt'	=> $request->get('alt'),
+			'speed'	=> $request->get('speed'),
+			'time'	=> $request->get('time'),
+		);
+
+		$gps_entry_db = add_entry($gps_entry);
+
+		return new Response(json_encode($gps_entry_db), HTTP_CREATED);
+	}
+
+	public function getEntry(Application $app, $id) {
+		$gps = $app['pdo']->fetchAssoc('SELECT * FROM gps WHERE id = ?', $id);
+
+		if (0) {
+			$app->abort(HTTP_NOT_FOUND, "Parameter {$param} does not exist.");
+		}
+		return json_encode($gps);
+	}
+
+	private function add_entry(Application $app, $gps) {
+		$app['pdo']->insert('gps', $gps);
+
+		return 0;
 	}
 }
 
